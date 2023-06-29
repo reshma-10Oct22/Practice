5a47880a58d3c71ae3d1c9f73cc85727fb3827dd
reshma-10Oct22 <reshma.hs@zohocorp.com>
2023-06-29 10:44:28 +0530

    Unit Testing

--- test/valid_paranthesis_test.dart
+++ test/valid_paranthesis_test.dart
@@ -1,8 +1,35 @@
 import 'package:valid_paranthesis/valid_paranthesis.dart';
 import 'package:test/test.dart';
 
+import '../bin/valid_paranthesis.dart';
+
 void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
+  // Valid Parnthesis
+  test('Valid', () {
+    expect(isValid("()"), true);
+  });
+
+  test('Valid', () {
+    expect(isValid("[]"), true);
+  });
+
+  test('Valid', () {
+    expect(isValid("{}"), true);
+  });
+  test('Valid', () {
+    expect(isValid("({[]})"), true);
+  });
+
+  // Invalid Paranthesis
+
+  test('Invalid', () {
+    expect(isValid("}"), false);
+  });
+  test('Invalid', () {
+    expect(isValid("(}"), false);
+  });
+
+  test('Invalid', () {
+    expect(isValid("([)]"), false);
+  });
 }
